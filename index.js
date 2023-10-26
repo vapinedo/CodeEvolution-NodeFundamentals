04676e700f4cd4212f156bbfdad37ed7bcae24a5
Victor Pinedo <victor.pineda@linea.ai>
2023-10-26 13:00:37 -0500

    Importing JSON files

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const SuperHero = require('./super-hero');
-
-const batman = new SuperHero('Batman');
-console.log(batman.getName());
-batman.setName('Bruce Wayne');
-console.log(batman.getName());
-
-const superman = new SuperHero('Superman');
-console.log(superman.getName());
+const data = require('./data.json');
+console.log(data.name);
