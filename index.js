72fce580aa40eb360074aa25bdd94d6e7a82687d
Victor Pinedo <victor.pineda@linea.ai>
2023-10-26 08:56:15 -0500

    Module Scope in Node JS

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-// const math = require("./math");
-// const sum = math.add(2,3);
-// const sub = math.subtract(7,3);
-// console.log("Sum is", sum);
-// console.log("Subtract is", sub);
-
-const { add, subtract } = require("./math");
-const sum = add(2,3);
-const sub = subtract(7,3);
-console.log("Sum is", sum);
-console.log("Subtract is", sub);
+require('./batman');
+require('./superman');
