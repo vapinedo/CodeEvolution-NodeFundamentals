fb5b69ad513c7af5fa6e4b42560834fc0835e86d
Victor Pinedo <victor.pineda@linea.ai>
2023-10-29 10:08:33 -0500

    HTTP Routing

--- built-in-modules/http.js
+++ built-in-modules/http.js
@@ -2,12 +2,23 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    const name = 'Valp';
-    res.writeHead(200, { "Content-Type": "text/html" });
-    const filePath = __dirname + '/index.html';
-    let html = fs.readFileSync(filePath, 'utf-8');
-    html = html.replace("{{name}}", name);
-    res.end(html);
+    // req.method GET, POST, PUT, DELETE, PATCH
+    if (req.url === '/') {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Home Page");
+    } else if (req.url === '/about') {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("About Page");
+    } else if (req.url === '/api') {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({
+            firstname: "Bruce",
+            lastname: "Wayne"
+        }));
+    } else {
+        res.writeHead(404);
+        res.end("Page not found");
+    }
 });
 
 server.listen(3000, () => {
