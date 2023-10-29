e43ed890a38f991ebe87947c3dc3fc37b6d8d162
Victor Pinedo <victor.pineda@linea.ai>
2023-10-29 09:42:32 -0500

    HTML Template

--- built-in-modules/http.js
+++ built-in-modules/http.js
@@ -2,11 +2,12 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
+    const name = 'Valp';
     res.writeHead(200, { "Content-Type": "text/html" });
     const filePath = __dirname + '/index.html';
-    // const html = fs.readFileSync(filePath, 'utf-8');
-    // res.end(html);
-    fs.createReadStream(filePath).pipe(res);
+    let html = fs.readFileSync(filePath, 'utf-8');
+    html = html.replace("{{name}}", name);
+    res.end(html);
 });
 
 server.listen(3000, () => {
