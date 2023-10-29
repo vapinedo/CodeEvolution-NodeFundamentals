1553d7775c7aec2ca13ef061c190a6eb718d8a99
Victor Pinedo <victor.pineda@linea.ai>
2023-10-29 08:46:58 -0500

    JSON Response

--- built-in-modules/http.js
+++ built-in-modules/http.js
@@ -1,8 +1,12 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end('Hello World');
+    const superHero = {
+        firstname: "Bruce",
+        lastname: "Wayne"
+    };
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(superHero));
 });
 
 server.listen(3000, () => {
