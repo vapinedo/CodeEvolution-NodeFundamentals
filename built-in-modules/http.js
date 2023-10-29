1bc2859fd6450ea7cf6161f350a1c003ffe83116
Victor Pinedo <victor.pineda@linea.ai>
2023-10-29 09:07:11 -0500

    HTML Response

--- built-in-modules/http.js
+++ built-in-modules/http.js
@@ -1,12 +1,12 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    const superHero = {
-        firstname: "Bruce",
-        lastname: "Wayne"
-    };
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(superHero));
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const filePath = __dirname + '/index.html';
+    // const html = fs.readFileSync(filePath, 'utf-8');
+    // res.end(html);
+    fs.createReadStream(filePath).pipe(res);
 });
 
 server.listen(3000, () => {
