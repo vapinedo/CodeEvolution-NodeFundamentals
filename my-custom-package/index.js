4fd56027f4319c9a6ea54bd9e06ecd697d04dcbf
Victor Pinedo <victor.pineda@linea.ai>
2023-10-31 14:59:58 -0500

    Using Packages

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,4 +1,8 @@
+const upperCase = require('upper-case').upperCase;
+
 function greet(name) {
-    console.log('Hello ' + name);
+    console.log(upperCase('Hello ' + name));
 }
+greet('Victor Pinedo');
+
 module.exports = greet;
