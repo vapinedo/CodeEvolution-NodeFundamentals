cbb46728af7e9ae03a2b142261f0a744fbb1b075
Victor Pinedo <victor.pineda@linea.ai>
2023-11-01 09:14:14 -0500

    Fix impor statement

--- my-custom-package/index.mjs
+++ my-custom-package/index.mjs
@@ -1,8 +1,8 @@
-const upperCase = require('upper-case').upperCase;
+import { upperCase } from 'upper-case';
 
 function greet(name) {
     console.log(upperCase('Hello ' + name));
 }
 greet('Victor Pinedo');
 
-module.exports = greet;
+export default greet;
